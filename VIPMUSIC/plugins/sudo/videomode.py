96403776c68b5386e1330fea2a2d8ed4b4066474
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-09-25 22:19:26 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- VIPMUSIC/plugins/sudo/videomode.py
+++ VIPMUSIC/plugins/sudo/videomode.py
@@ -9,6 +9,7 @@
 #
 
 from pyrogram import filters
+from pyrogram.types import Message
 
 import config
 from strings import get_command
@@ -18,22 +19,22 @@ from VIPMUSIC.utils.database import add_off, add_on
 from VIPMUSIC.utils.decorators.language import language
 
 # Commands
-LOGGER_COMMAND = get_command("LOGGER_COMMAND")
+VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
 
 
-@app.on_message(filters.command(LOGGER_COMMAND) & SUDOERS)
+@app.on_message(filters.command(VIDEOMODE_COMMAND) & SUDOERS)
 @language
-async def logger(client, message, _):
-    usage = _["log_1"]
+async def videoloaymode(client, message: Message, _):
+    usage = _["vidmode_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
-        await add_on(config.LOG)
-        await message.reply_text(_["log_2"])
-    elif state == "disable":
-        await add_off(config.LOG)
-        await message.reply_text(_["log_3"])
+    if state == "download":
+        await add_on(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_2"])
+    elif state == "m3u8":
+        await add_off(config.YTDOWNLOADER)
+        await message.reply_text(_["vidmode_3"])
     else:
         await message.reply_text(usage)
