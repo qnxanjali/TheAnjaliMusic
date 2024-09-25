04a983e8f07ea8ff0f01034d489d939ebe225f5c
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-09-25 21:53:23 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 by qnxanjal@Github, < https://github.com/qnxanjal >.
+#
+# This file is part of < https://github.com/qnxanjali/TheAnjali > project,
+# and is released under the MIT License.
+# Please see < https://github.com/qnxanjali/TheAnjali/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .config import *
