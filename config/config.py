97d1c2ece98c7fe52f06029cc49abdcc35f533b5
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-09-26 07:30:34 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- config/config.py
+++ config/config.py
@@ -89,7 +89,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/qnxanjali/TheAnjali-",
+    "https://github.com/qnxanjali/TheAnjali",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
