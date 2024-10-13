405feb6e26fdfbf4cd81a2f77267beaf0fef7ffe
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-13 11:47:53 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

--- config/config.py
+++ config/config.py
@@ -111,11 +111,11 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/ANJALINETWORK"
 )  # Example:- https://t.me/nenobots
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/+EgyhXPWmSBNiZGU1"
+    "SUPPORT_GROUP", "https://t.me/+ths-Tywxzcg2ZTFl"
 )  # Example:- https://t.me/ur_support07
 
 SUPPORT_CHAT = getenv(
-    "SUPPORT_GROUP", "https://t.me/ANJALIWORLD"
+    "SUPPORT_GROUP", "https://t.me/RBGOFFICIAL1"
 )  # Example:- https://t.me/TG_FRIENDSS
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
